13908030844bfc9122bb18a787e9766f40886ffe
Fatima Gomez <fatima.gomez@datamade.us>
2020-05-01 11:44:57 -0500

    clean up windowed bill scraping

--- dags/windowed_bill_scraping.py
+++ dags/windowed_bill_scraping.py
@@ -12,28 +12,27 @@ default_args = {
 }
 
 dag = DAG(
-    'daily_scraping',
+    'windowed_bill_scraping',
     default_args=default_args,
     schedule_interval=None # Eventually 5,20,35,50 * * * 0-6
 )
 
-def mult_five_past_hour_scraping():
-    print("stuff")
+def windowed_bill_scraping():
     # SUNDAY THROUGH SATURDAY
-        # 9pm FRIDAY through 5am Saturday, only run at 35,50 minutes
-        now = datetime.now()
-        if now.weekday == 5 and now.hour >= 9 and now.minute < 35:
-            pass
-        elif now.weekday == 6 and now.hour <= 5 and now.minute < 35:
-            pass
-        else:
-            # 5,20,35,50 * * * 0-4 datamade /usr/bin/flock -n /tmp/metrobills.lock -c "WINDOW=0.05 $APPDIR/scripts/lametro/windowed-bill-scrape.sh" >> /tmp/lametro.log
-            subprocess.run('$APPDIR/scripts/lametro/windowed-bill-scrape.sh', capture_output=True)
+    # 9pm FRIDAY through 5am SATURDAY, only run at 35,50 minutes
+    now = datetime.now()
+    if now.weekday == 5 and now.hour >= 9 and now.minute < 35:
+        pass
+    elif now.weekday == 6 and now.hour <= 5 and now.minute < 35:
+        pass
+    else:
+        # datamade /usr/bin/flock -n /tmp/metrobills.lock -c "WINDOW=0.05 $APPDIR/scripts/lametro/windowed-bill-scrape.sh" >> /tmp/lametro.log
+        subprocess.run('$APPDIR/scripts/lametro/windowed-bill-scrape.sh', capture_output=True)
 
 
 
 t1 = DjangoOperator(
-    task_id='mult_five_past_hour_scraping',
+    task_id='windowed_bill_scraping',
     dag=dag,
-    python_callable=mult_five_past_hour_scraping
+    python_callable=windowed_bill_scraping
 )
