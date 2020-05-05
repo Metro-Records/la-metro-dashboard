524989d707328ee7c25fbe478977b80a9f9d224a
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-05-05 12:05:15 -0500

    Enable authentication and remote logging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 if [ "$AIRFLOW_MIGRATE" = 'on' ]; then
     airflow initdb
+    if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM users"` -eq "0" ]; then
+        python scripts/init_db.py
+    fi
 fi
+
 
 exec "$@"
