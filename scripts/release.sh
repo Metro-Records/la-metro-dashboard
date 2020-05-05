66261f394052dafdcdf756f038607ae65515f0a9
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-05-05 12:05:15 -0500

    Enable authentication and remote logging

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+set -euo pipefail
+
+# Run migrations.
 airflow initdb
+
+# Check if initial data exists, and if not, run initial imports.
+if [ `psql ${DATABASE_URL} -tAX -c "SELECT COUNT(*) FROM users"` -eq "0" ]; then
+    python scripts/init_db.py
+fi
