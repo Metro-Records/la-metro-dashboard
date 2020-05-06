c673aa3e1a2d70f1f19f6bc3ecb32860e3af9341
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-05-06 11:56:49 -0500

    Add EC2 deployment configs

---
+++ scripts/app_start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+# Make sure the deployment group specific variables are available to this
+# script.
+source ${BASH_SOURCE%/*}/../configs/$DEPLOYMENT_GROUP_NAME-config.conf
+
+# Re-read supervisor config, and add new processes
+echo "Reloading supervisor"
+supervisorctl reread
+supervisorctl start $APP_NAME
+
+echo "Reloading nginx"
+nginx -t
+service nginx reload || service nginx start
