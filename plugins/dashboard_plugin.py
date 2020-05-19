9d63cfdfc0c5e52de04417a99ad59d0a78ff9f2b
Fatima Gomez <fatima.gomez@datamade.us>
2020-05-19 17:00:14 -0500

    first two columns of table - name and state of most recent run

--- plugins/dashboard_plugin.py
+++ plugins/dashboard_plugin.py
@@ -1,5 +1,8 @@
+from airflow import settings
+from airflow.models import dag, dagrun, taskinstance
 from airflow.models.dagbag import DagBag
 from airflow.plugins_manager import AirflowPlugin
+
 from flask import Blueprint
 from flask_admin import BaseView, expose
 
@@ -7,10 +10,27 @@ from flask_admin import BaseView, expose
 class DashboardView(BaseView):
     @expose('/')
     def index(self):
-        dagbag = DagBag()
-        dag_ids = dagbag.dag_ids
-        all_dags = [dagbag.get_dag(dag_id) for dag_id in dag_ids]
-        return self.render('dashboard.html', data=all_dags)
+        bag = DagBag()
+        all_dag_ids = bag.dag_ids
+        all_dags = [bag.get_dag(dag_id) for dag_id in all_dag_ids]
+
+        data = []
+
+        for d in all_dags:
+            session = settings.Session()
+            last_run = dag.get_last_dagrun(d.dag_id, session,include_externally_triggered=True) 
+            state = last_run.get_state() if last_run else 'Has Not Run'
+            current_task = [ti for ti in last_run.get_task_instances()] if last_run else 'N/A'
+
+            dag_info = {
+                'name': d.dag_id,
+                'state': state,
+                'current_task': current_task
+                # 'last_run':
+                # 'next_scheduled':
+            }
+            data.append(dag_info)
+        return self.render('dashboard.html', data=data)
 
 
 admin_view_ = DashboardView(category='Dashboard Plugin', name='Dashboard View')
