d0710b54ec846d357d5b2defade0493789154816
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-06-09 10:19:33 -0500

    Use local cache dir for pupa scraping

--- dags/scripts/targeted-scrape.sh
+++ dags/scripts/targeted-scrape.sh
@@ -7,5 +7,5 @@ RPM={{params.rpm}}
 SCRAPERS_DIR_PATH={{params.scrapers_dir_path}}
 
 cd $SCRAPERS_DIR_PATH
-pupa update --datadir=/cache/events/_data/ lametro --scrape $TARGET window=$WINDOW --rpm=$RPM
+pupa update --datadir=$SCRAPERS_DIR_PATH/cache/events/_data/ lametro --scrape $TARGET window=$WINDOW --rpm=$RPM
 pupa update lametro --import
