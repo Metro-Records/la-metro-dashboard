7d457ece8013a2c1977c9c92267397e89f46047a
Jean Cochrane <jeancochrane@users.noreply.github.com>
2020-06-11 16:39:07 -0500

    Use TARGET for cache targeted-scrape.sh
    Co-authored-by: hannah cushman garland <hancush@users.noreply.github.com>

--- dags/scripts/targeted-scrape.sh
+++ dags/scripts/targeted-scrape.sh
@@ -8,5 +8,5 @@ SCRAPERS_DIR_PATH={{params.scrapers_dir_path}}
 LA_METRO_DATABASE_URL={{params.la_metro_database_url}}
 
 cd $SCRAPERS_DIR_PATH
-pupa update --datadir=$SCRAPERS_DIR_PATH/cache/events/_data/ lametro --scrape $TARGET window=$WINDOW --rpm=$RPM
+pupa update --datadir=$SCRAPERS_DIR_PATH/cache/$TARGET/_data/ lametro --scrape $TARGET window=$WINDOW --rpm=$RPM
 DATABASE_URL=$LA_METRO_DATABASE_URL pupa update lametro --import
