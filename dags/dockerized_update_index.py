555b77fa0c58ed9cd1a3a3b6f9626c99740dc008
hancush <hannah.cushman@gmail.com>
2020-07-09 10:04:16 -0500

    Add Dockerized update_index DAG, update local dev env to support

--- dags/dockerized_update_index.py
+++ dags/dockerized_update_index.py
@@ -6,12 +6,18 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.docker_operator import DockerOperator
 
 
-LA_METRO_DIR_PATH = os.getenv('LA_METRO_DIR_PATH', '/la-metro-councilmatic/')
-
 default_args = {
     'start_date': datetime.now() - timedelta(hours=1),
-    'execution_timeout': timedelta(minutes=1)
+    'execution_timeout': timedelta(minutes=15)
 }
+
+LA_METRO_DIR_PATH = os.getenv('LA_METRO_DIR_PATH', '/la-metro-councilmatic/')
+LA_METRO_DATABASE_URL = os.getenv('LA_METRO_DATABASE_URL', 'postgres://postgres:postgres@postgres:5432/lametro')
+LA_METRO_SOLR_URL = os.getenv('LA_METRO_SOLR_URL', 'http://solr:8983/solr/lametro')
+
+# Configure DAG container to connect to specific Docker network, useful for
+# local development (not necessary in production)
+DOCKER_NETWORK = os.getenv('DOCKER_NETWORK', None)
 
 with DAG('dockerized_update_index', default_args=default_args, schedule_interval='0 1 * * *') as dag:
 
@@ -23,7 +29,12 @@ with DAG('dockerized_update_index', default_args=default_args, schedule_interval
     t2 = DockerOperator(
         task_id='update_index',
         image='lametro:latest',
-        command='python manage.py update_index --batch-size=100'
+        command='python manage.py update_index --batch-size=100',
+        environment={
+            'LA_METRO_DATABASE_URL': LA_METRO_DATABASE_URL,
+            'LA_METRO_SOLR_URL': LA_METRO_SOLR_URL,
+        },
+        network_mode=DOCKER_NETWORK
     )
 
     t1 >> t2
