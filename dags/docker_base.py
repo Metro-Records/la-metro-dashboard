cec53a114fec705060699e37483c3f4317da721b
hancush <hannah.cushman@gmail.com>
2020-07-10 15:34:57 -0500

    Spike abstracting base Docker functionality out

---
+++ dags/docker_base.py
@@ -0,0 +1,49 @@
+from datetime import datetime, timedelta
+import os
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+# Use backported operator containing bug fix:
+# - https://github.com/apache/airflow/issues/8629#issuecomment-641461423
+# - https://pypi.org/project/apache-airflow-backport-providers-docker/
+from airflow.providers.docker.operators.docker import DockerOperator
+
+
+class GPGDockerOperator(DockerOperator):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        # Configure DAG container to connect to specific Docker network, useful for
+        # local development (not necessary in production)
+        DOCKER_NETWORK = os.getenv('DOCKER_NETWORK', None)
+
+        # Configure the location of the GPG keyring to mount into the container for
+        # decrypting secrets
+        GPG_KEYRING_PATH = os.getenv('GPG_KEYRING_PATH', '/home/datamade/.gnupg')
+
+        # Configure the Airflow directory path. For local development, this should
+        # be the root directory of your Airflow project. This has to be configured
+        # because the Docker daemon creates containers from the host, not from within
+        # the application container.
+        AIRFLOW_DIR_PATH = os.getenv(
+            'AIRFLOW_DIR_PATH',
+            os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
+        )
+
+        self.volumes += [
+            '{}:/root/.gnupg'.format(GPG_KEYRING_PATH),
+            '{}:/app/airflow_configs'.format(os.path.join(AIRFLOW_DIR_PATH, 'configs')),
+            '{}:/app/airflow_scripts'.format(os.path.join(AIRFLOW_DIR_PATH, 'scripts'))
+        ]
+
+        if not self.network_mode:  # Give DAG-configured network precedence
+            self.network_mode = DOCKER_NETWORK
+
+        self.force_pull = True
+
+        # TODO: Is this safe? (In other words, can self.command be something
+        # that would break this?)
+        self.command = '/bin/bash -ce "airflow_scripts/concat_settings.sh; {}"'.format(self.command)
--- dags/dockerized_update_index.py
+++ dags/dockerized_update_index.py
@@ -2,12 +2,8 @@ from datetime import datetime, timedelta
 import os
 
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
 
-# Use backported operator containing bug fix:
-# - https://github.com/apache/airflow/issues/8629#issuecomment-641461423
-# - https://pypi.org/project/apache-airflow-backport-providers-docker/
-from airflow.providers.docker.operators.docker import DockerOperator
+from dags.docker_base import GPGDockerOperator
 
 
 default_args = {
@@ -19,40 +15,14 @@ LA_METRO_DIR_PATH = os.getenv('LA_METRO_DIR_PATH', '/la-metro-councilmatic/')
 LA_METRO_DATABASE_URL = os.getenv('LA_METRO_DATABASE_URL', 'postgres://postgres:postgres@postgres:5432/lametro')
 LA_METRO_SOLR_URL = os.getenv('LA_METRO_SOLR_URL', 'http://solr:8983/solr/lametro')
 
-# Configure DAG container to connect to specific Docker network, useful for
-# local development (not necessary in production)
-DOCKER_NETWORK = os.getenv('DOCKER_NETWORK', None)
-
-# Configure the location of the GPG keyring to mount into the container for
-# decrypting secrets
-GPG_KEYRING_PATH = os.getenv('GPG_KEYRING_PATH', '/home/datamade/.gnupg')
-
-# Configure the Airflow directory path. For local development, this should
-# be the root directory of your Airflow project. This has to be configured
-# because the Docker daemon creates containers from the host, not from within
-# the application container.
-AIRFLOW_DIR_PATH = os.getenv(
-    'AIRFLOW_DIR_PATH',
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), '..')
-)
-
-print('\n'.join([AIRFLOW_DIR_PATH, GPG_KEYRING_PATH]))
-
 with DAG('dockerized_update_index', default_args=default_args, schedule_interval='0 1 * * *') as dag:
 
-    t1 = DockerOperator(
+    t1 = GPGDockerOperator(
         task_id='update_index',
         image='datamade/la-metro-councilmatic:staging',
-        command="/bin/bash -ce 'airflow_scripts/concat_settings.sh; python manage.py update_index --batch-size=100 --age=90'",
+        command="python manage.py update_index --batch-size=100 --age=90",
         environment={
             'LA_METRO_DATABASE_URL': LA_METRO_DATABASE_URL,
             'LA_METRO_SOLR_URL': LA_METRO_SOLR_URL,
         },
-        volumes=[
-            '{}:/root/.gnupg'.format(GPG_KEYRING_PATH),
-            '{}:/app/airflow_configs'.format(os.path.join(AIRFLOW_DIR_PATH, 'configs')),
-            '{}:/app/airflow_scripts'.format(os.path.join(AIRFLOW_DIR_PATH, 'scripts'))
-        ],
-        network_mode=DOCKER_NETWORK,
-        force_pull=True,
     )
