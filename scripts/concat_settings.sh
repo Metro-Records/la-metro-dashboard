6f427a75b02cc8bf4c867f5a9502721213a95dba
hancush <hannah.cushman@gmail.com>
2020-07-14 15:56:25 -0500

    Refactor daily_scraping to run in container

--- scripts/concat_settings.sh
+++ scripts/concat_settings.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-echo $DECRYPTED_SETTINGS
-echo $DESTINATION_SETTINGS
-
 blackbox_postdeploy
 
 if [ "$DECRYPTED_SETTINGS" != "$DESTINATION_SETTINGS" ]; then
@@ -11,5 +8,3 @@ if [ "$DECRYPTED_SETTINGS" != "$DESTINATION_SETTINGS" ]; then
 fi
 
 cat airflow_configs/connection_settings.py >> $DESTINATION_SETTINGS
-
-cat $DESTINATION_SETTINGS
