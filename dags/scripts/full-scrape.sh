d507f33e229fd5ee32fa1a0cf951ef9e48b2d7ae
hancush <hannah.cushman@gmail.com>
2020-08-25 08:37:55 -0500

    Rely on fewer assumptions

--- dags/scripts/full-scrape.sh
+++ dags/scripts/full-scrape.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 
-import_to_staging() {
-    # If the database URL does not contain "staging", it's the production
-    # database. Append "_staging" and import. Assumes that the staging and
-    # production databases live on the same server, and follow the naming
-    # convention "${DATABASE_NAME}" and "${DATABASE_NAME}_staging".
-    if [[ "$DATABASE_URL" != *"staging"* ]]; then
-        STAGING_DATABASE_URL="${LA_METRO_DATABASE_URL}_staging"
-        echo "Importing into staging database ${STAGING_DATABASE_URL}"
-        SHARED_DB=True DATABASE_URL=$STAGING_DATABASE_URL pupa update lametro --import || echo "${STAGING_DATABASE_URL} does not exist"
+conditionally_import_to_staging() {
+    if [[ -n "$LA_METRO_STAGING_DATABASE_URL" ]]; then
+        echo "Importing into staging database ${LA_METRO_STAGING_DATABASE_URL}"
+        SHARED_DB=True DATABASE_URL=$LA_METRO_STAGING_DATABASE_URL pupa update lametro --import || echo "${LA_METRO_STAGING_DATABASE_URL} does not exist"
     fi
 }
 
@@ -17,9 +12,9 @@ import_to_staging() {
 # windowed bills.
 pupa update lametro --scrape
 SHARED_DB=True DATABASE_URL=$LA_METRO_DATABASE_URL pupa update lametro --import
-import_to_staging
+conditionally_import_to_staging
 
 # Scrape all bills.
 pupa update lametro --scrape bills window=0
 SHARED_DB=True DATABASE_URL=$LA_METRO_DATABASE_URL pupa update lametro --import
-import_to_staging
+conditionally_import_to_staging
