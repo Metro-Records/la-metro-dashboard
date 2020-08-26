74197a9d8b9bb72eff9af988664e739dbb87f074
hancush <hannah.cushman@gmail.com>
2020-08-26 09:30:39 -0500

    Start conditional on process groups existing

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -8,7 +8,13 @@ source ${BASH_SOURCE%/*}/../configs/$DEPLOYMENT_GROUP_NAME-config.conf
 # Re-read supervisor config, and add new processes
 echo "Reloading supervisor"
 supervisorctl reread $APP_NAME $APP_NAME-scheduler
-supervisorctl add $APP_NAME $APP_NAME-scheduler || supervisorctl start $APP_NAME $APP_NAME-scheduler
+
+# Add or start the dashboard process groups. Add does not throw a non-zero exit
+# code if the process group exists, so grep the add output for the message
+# indicating the groups have already been added and conditionally start them.
+# (Adding a new process group starts it, so it is redundant to call start in
+# that case.)
+supervisorctl add $APP_NAME $APP_NAME-scheduler | grep 'process group already active' && supervisorctl start $APP_NAME $APP_NAME-scheduler
 
 echo "Reloading nginx"
 nginx -t
