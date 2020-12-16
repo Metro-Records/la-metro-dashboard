dcf849f9af29bbcbbf4c19ff32257394ec3c6105
hancush <hannah.cushman@gmail.com>
2020-12-16 15:30:12 -0600

    Set environment variable referenced in last DATABASE_URL in pupa_settings.py, connects #58

--- dags/scripts/targeted-scrape.sh
+++ dags/scripts/targeted-scrape.sh
@@ -4,16 +4,16 @@ set -e
 conditionally_import_to_staging() {
     if [[ -n "$LA_METRO_STAGING_DATABASE_URL" ]]; then
         echo "Importing into staging database ${LA_METRO_STAGING_DATABASE_URL}"
-        SHARED_DB=True DATABASE_URL=$LA_METRO_STAGING_DATABASE_URL pupa update lametro --import || echo "${LA_METRO_STAGING_DATABASE_URL} does not exist"
+        SHARED_DB=True LA_METRO_DATABASE_URL=$LA_METRO_STAGING_DATABASE_URL pupa update lametro --import || echo "${LA_METRO_STAGING_DATABASE_URL} does not exist"
     fi
 }
 
 if [[ -n "$WINDOW" ]]; then
-    SHARED_DB=True DATABASE_URL=$LA_METRO_DATABASE_URL \
+    SHARED_DB=True \
         pupa update --datadir=/cache/$TARGET/_data/ lametro $TARGET \
         window=$WINDOW --rpm=$RPM
 else
-    SHARED_DB=True DATABASE_URL=$LA_METRO_DATABASE_URL \
+    SHARED_DB=True \
         pupa update --datadir=/cache/$TARGET/_data/ lametro $TARGET \
         --rpm=$RPM
 fi
