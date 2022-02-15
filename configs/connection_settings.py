b166ef90e2654f917c20202d1f30915d3d2cb1e9
hancush <hannah.cushman@gmail.com>
2022-02-15 11:45:12 -0600

    Add lametro logging config

--- configs/connection_settings.py
+++ configs/connection_settings.py
@@ -2,6 +2,8 @@
 Override Postgres and Haystack connections for the scrapers and LA Metro
 containers.
 '''
+import sys
+
 import dj_database_url
 
 
@@ -20,4 +22,34 @@ HAYSTACK_CONNECTIONS = {
         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
         'URL': os.getenv('LA_METRO_SOLR_URL', 'http://solr:8983/solr/lametro'),
     },
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            'format': '[%(asctime)s][%(levelname)s] %(name)s '
+                      '%(filename)s:%(funcName)s:%(lineno)d | %(message)s',
+            'datefmt': '%H:%M:%S',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+            'stream': sys.stdout
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG'
+    },
+    'loggers': {
+        'lametro': {
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    }
 }
