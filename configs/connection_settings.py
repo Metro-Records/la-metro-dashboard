98fd7cd6621a59a31ff6dfa4376c485e88ecfd33
hancush <hannah.cushman@gmail.com>
2022-02-21 10:42:12 -0600

    Move logging settings upstream

--- configs/connection_settings.py
+++ configs/connection_settings.py
@@ -23,34 +23,3 @@ HAYSTACK_CONNECTIONS = {
         'URL': os.getenv('LA_METRO_SOLR_URL', 'http://solr:8983/solr/lametro'),
     },
 }
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'console': {
-            'format': '[%(asctime)s][%(levelname)s] %(name)s '
-                      '%(filename)s:%(funcName)s:%(lineno)d | %(message)s',
-            'datefmt': '%H:%M:%S',
-        },
-    },
-    'handlers': {
-        'default': {},
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'console',
-            'stream': sys.stdout
-        },
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG'
-    },
-    'loggers': {
-        'lametro': {
-            'handlers': ['console'],
-            'propagate': False,
-        },
-    }
-}
