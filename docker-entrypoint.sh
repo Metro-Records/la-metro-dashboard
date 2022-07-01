04a0ef8659da94ea5caa0e7d35da4be27848a44e
msj <monkruman.st.jules@datamde.us>
2022-07-01 11:13:42 -0400

    Address deprecation warnings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$AIRFLOW_MIGRATE" = 'on' ]; then
     airflow db init
-    if [ `psql ${AIRFLOW__CORE__SQL_ALCHEMY_CONN} -tAX -c "SELECT COUNT(*) FROM ab_user"` -eq "0" ]; then
+    if [ `psql ${AIRFLOW__DATABASE__SQL_ALCHEMY_CONN} -tAX -c "SELECT COUNT(*) FROM ab_user"` -eq "0" ]; then
         airflow create_user -r Admin -u ${AIRFLOW_USERNAME} -p ${AIRFLOW_PASSWORD} -e team@datamade.us -f Airflow -l Admin
     fi
 fi
