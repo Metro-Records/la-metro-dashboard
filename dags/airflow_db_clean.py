e197e7713f50082087496a3be38440c7d4231e44
msj <monkruman.st.jules@datamde.us>
2023-07-06 11:58:13 -0400

    Add new maintenance dags

---
+++ dags/airflow_db_clean.py
@@ -0,0 +1,20 @@
+"""
+A maintenance workflow to clean old metadata from the Airflow database.
+"""
+
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+
+START_DATE = datetime(2023, 7, 1)
+
+DELETE_BEFORE = datetime.now() - timedelta(days=30)
+
+bash_command = f"airflow db clean -y --clean-before-timestamp '{DELETE_BEFORE}'"
+
+with DAG(
+    dag_id="airflow_db_cleanup",
+    start_date=START_DATE,
+    schedule="@monthly",
+):
+    BashOperator(task_id="task", bash_command=bash_command)
